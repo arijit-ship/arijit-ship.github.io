259581bb625cffe1f3ab1818e747cbc91aec564a
Arijit Das <56244823+arijit-ship@users.noreply.github.com>
2020-07-11 18:20:54 +0530

    Delete tempCodeRunnerFile.js

--- opencalc_web_assets/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-for(i of  raw_input_string){
-
-    if(i=="×"){
-      input=raw_input_string.replace(/×/g,"*");
-    }
-    else if(i=="√"){
-      input=input.replace("√","math.sqrt()")
-    }
-    
