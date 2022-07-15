f32dbdbfcf84b9a62fdb3a51b22c8f3c3768e67d
ARIJIT DAS <dasarijit1st@gmail.com>
2022-07-15 21:55:01 +0530

    C++ mdbook uploaded

---
+++ c_plusplus_notes_masters_physics/custom.js
@@ -0,0 +1,4 @@
+var img = ["https://arijit-ship.github.io/Haskell-notes-webpage-assets/demon-slayer-bg.jpg", "https://arijit-ship.github.io/Haskell-notes-webpage-assets/samuraichampllo_png.png", "https://w7.pngwing.com/pngs/442/409/png-transparent-durarara-x1-anime-izaya-orihara-durarara-vol-6-light-novel-headache-black-hair-manga-chibi-thumbnail.png"]
+
+document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL3kr8OIL9kzhrn_PTOMrPqS-BwVzQFGDQxQ&usqp=CAU')";
+console.log("custom.js");
