3abfcba8ed45ffe5a39735b33c258b6d9e4ad8a1
ARIJIT DAS <dasarijit1st@gmail.com>
2022-07-15 22:04:48 +0530

    C++ mdbook update

--- c_plusplus_notes_masters_physics/custom.js
+++ c_plusplus_notes_masters_physics/custom.js
@@ -1,4 +1,17 @@
-var img = ["https://arijit-ship.github.io/Haskell-notes-webpage-assets/demon-slayer-bg.jpg", "https://arijit-ship.github.io/Haskell-notes-webpage-assets/samuraichampllo_png.png", "https://w7.pngwing.com/pngs/442/409/png-transparent-durarara-x1-anime-izaya-orihara-durarara-vol-6-light-novel-headache-black-hair-manga-chibi-thumbnail.png"]
 
-document.body.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL3kr8OIL9kzhrn_PTOMrPqS-BwVzQFGDQxQ&usqp=CAU')";
-console.log("custom.js");
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var img = [
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/demon-slayer-bg.jpg?raw=true", 
+"https://arijit-ship.github.io/Haskell-notes-webpage-assets/samuraichampllo_png.png", 
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/62d1938d4052a.jpg?raw=true", 
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/kekkaishi-43_le-retour-de-la-tempeteavi_000361027.jpg?raw=true"
+]
+
+bg = img[getRandomInt(0, img.length - 1)];
+
+document.body.style.backgroundImage = "url("+ bg +  ")";
