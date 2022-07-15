037a31d0f4744f1c684dae62610b51bbe67f0ab3
ARIJIT DAS <dasarijit1st@gmail.com>
2022-07-15 22:43:14 +0530

    C++ mdbook update

---
+++ c_plusplus_notes_masters_physics/book/custom.js
@@ -0,0 +1,23 @@
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var img = [
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/36626.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/5-centimeters-per-second-2007-ending-1024x576.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/HD-wallpaper-the-garden-of-words-male-movie-kotoha-winter-makoto-snow-anime-love-garden-shinkai.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/demon-slayer-bg.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/scene-12.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/scene-22.jpg?raw=true",
+"https://github.com/arijit-ship/arijit-ship.github.io/blob/master/c_plusplus_notes_masters_physics/bg/silhouette_stairs_sun_213031_1366x768.jpg?raw=true"
+
+]
+
+bg = img[getRandomInt(0, img.length - 1)];
+
+document.body.style.backgroundImage = "url("+ bg +  ")";
+
+//document.body.style.backgroundImage = "url('../bg/scene-22.jpg')";
